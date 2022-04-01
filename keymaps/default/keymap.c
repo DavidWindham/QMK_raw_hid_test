6c398790580855d254ccfa38d378da8a580df7cf
DavidWindham <tinymouse2@gmail.com>
2022-04-01 17:10:53 +0100

    Modified the node test and set the keymap to send data when encoder is turned

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 
 #include "raw_hid.h"
 bool test_led_flip = false;
+uint8_t test_data[32];
 
 void matrix_scan_user(void) {
 	setPinOutput(B6);
@@ -15,8 +16,10 @@ void matrix_scan_user(void) {
 bool encoder_update_user(uint8_t idx, bool clockwise) {
 	if (idx == 0) {
 		if (clockwise) {
+			raw_hid_send(test_data, sizeof(test_data));
 			test_led_flip = false;
 		} else {
+			raw_hid_send(test_data, sizeof(test_data));
 			test_led_flip = true;
 		}
 	}
@@ -25,6 +28,7 @@ bool encoder_update_user(uint8_t idx, bool clockwise) {
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
 	test_led_flip = !test_led_flip;
+	raw_hid_send(data, length);
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
--- node_test.js
+++ node_test.js
@@ -2,10 +2,31 @@ var HID = require('node-hid');
 
 var keyboard = new HID.HID(61166, 1);
 
+let bufferedContent = Buffer.from('testthis');
+const outputLength = 32;
+const bufferedOutput = Buffer.concat([buf], outputLength);
+console.log("Buffered output:", bufferedOutput);
+
+//let uint_test = Uint32Array.from("test");
+//console.log("uint_test:", uint_test);
+
+
 function writeToHID(){
-	keyboard.write([0, 0]);
+	//console.log("Writing [1, 1]");
+	//keyboard.write([0, 0]);
+
+	console.log("Writing concat buffer");
+	keyboard.write(bufferedOutput);
+
+	//console.log("Writing uint_test");
+	//keyboard.write(uint_test);
+
+	//console.log("Writing array[32]);
+	//keyboard.write([74, 65, 73, 74, 79, 74, 68, 69, 73, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]);
+	
 	console.log("Written");
 	setTimeout(() => {writeToHID()}, 1000);
 }
 
 writeToHID();
+console.log("Setup complete");
